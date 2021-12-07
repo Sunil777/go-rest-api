92309f5c9485ac85202e6248d682ebe73b9910dd
sunilk <sunilk@one.com>
2021-12-07 14:57:00 +0530

    Initial code added

---
+++ pkg/mocks/book.go
@@ -0,0 +1,12 @@
+package mocks
+
+import "go-http-router/pkg/models"
+
+var Books = []models.Book{
+	{
+		Id:     1,
+		Title:  "Why I am an Atheist and Other Works",
+		Author: "Bhagat Singh",
+		Desc:   "Why I am an Atheist and Other Works",
+	},
+}
